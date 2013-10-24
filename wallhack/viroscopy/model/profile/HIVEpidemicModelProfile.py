fa43aa53b29774f0aa894764ff88db5041e01b59
Charanpal Dhanjal <charanpal@gmail.com>
2013-10-24 10:36:46 +0200

    Profiling of simulate + graph matching

--- wallhack/viroscopy/model/profile/HIVEpidemicModelProfile.py
+++ wallhack/viroscopy/model/profile/HIVEpidemicModelProfile.py
@@ -9,6 +9,9 @@ from wallhack.viroscopy.model.HIVEpidemicModel import HIVEpidemicModel
 from wallhack.viroscopy.model.HIVGraph import HIVGraph
 from wallhack.viroscopy.model.HIVRates import HIVRates
 from wallhack.viroscopy.model.HIVModelUtils import HIVModelUtils
+from wallhack.viroscopy.model.HIVVertices import HIVVertices
+from wallhack.viroscopy.model.HIVGraphMetrics2 import HIVGraphMetrics2
+from sandbox.misc.GraphMatch import GraphMatch
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 numpy.random.seed(24)
@@ -43,5 +46,35 @@ class HIVEpidemicModelProfile():
 
         ProfileUtils.profile('model.simulate()', globals(), locals())
 
+    def profileSimulateGraphMatch(self): 
+        N, matchAlpha, breakDist, purtScale = HIVModelUtils.toyABCParams()
+        startDate, endDate, recordStep, M, targetGraph = HIVModelUtils.toySimulationParams()
+        theta, stdTheta = HIVModelUtils.toyTheta()
+    
+        featureInds= numpy.ones(targetGraph.vlist.getNumFeatures(), numpy.bool)
+        featureInds[HIVVertices.dobIndex] = False 
+        featureInds[HIVVertices.infectionTimeIndex] = False 
+        featureInds[HIVVertices.hiddenDegreeIndex] = False 
+        featureInds[HIVVertices.stateIndex] = False 
+        featureInds = numpy.arange(featureInds.shape[0])[featureInds]        
+        
+        #QCV is fastest and most accurate 
+        #PATH is slowests but quite accurate 
+        #RANK is very fast by less accurate than PATH 
+        #U is fastest but least accurate         
+        
+        matcher = GraphMatch("QCV", alpha=matchAlpha, featureInds=featureInds, useWeightM=False)
+        matcher.lambdaM = 50 
+        matcher.init = "rand"
+        graphMetrics = HIVGraphMetrics2(targetGraph, breakDist, matcher, float(endDate))        
+        
+        def run(): 
+            times, infectedIndices, removedIndices, graph = HIVModelUtils.simulate(theta, startDate, endDate, recordStep, M, graphMetrics)
+            print("Mean distance " + str(graphMetrics.meanDistance()))
+        
+        ProfileUtils.profile('run()', globals(), locals())
+
+
 profiler = HIVEpidemicModelProfile()
-profiler.profileSimulate() #67.7
+#profiler.profileSimulate() #67.7
+profiler.profileSimulateGraphMatch() 
