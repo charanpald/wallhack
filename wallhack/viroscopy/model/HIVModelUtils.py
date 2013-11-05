25d899f8b403672cfddbd7aeb5c1b21aad9ab7a6
Charanpal Dhanjal <charanpal@gmail.com>
2013-11-05 10:22:18 +0100

    Increase  breakScale

--- wallhack/viroscopy/model/HIVModelUtils.py
+++ wallhack/viroscopy/model/HIVModelUtils.py
@@ -72,7 +72,7 @@ class HIVModelUtils(object):
     def realABCParams():
         N = 30 
         matchAlpha = 0.2 
-        breakScale = 1.2 
+        breakScale = 1.5 
         numEpsilons = 15
         epsilon = 0.8
         minEpsilon = 0.4
