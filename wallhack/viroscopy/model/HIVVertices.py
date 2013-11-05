9ad2e415d98e72a8b88dc71e821f814161cc4a98
Charanpal Dhanjal <charanpal@gmail.com>
2013-11-05 14:29:30 +0100

    addVertices and subList methods

--- wallhack/viroscopy/model/HIVVertices.py
+++ wallhack/viroscopy/model/HIVVertices.py
@@ -6,22 +6,36 @@ from apgl.util.Parameter import Parameter
 
 class HIVVertices(VertexList):
     def __init__(self, numVertices):
-        numFeatures = 8
-        super(HIVVertices, self).__init__(numVertices, numFeatures)
+        self.numFeatures = 8
+        super(HIVVertices, self).__init__(numVertices, self.numFeatures)
 
         #Need to randomly set up the initial values
-        self.V[:, self.dobIndex] = numpy.random.rand(numVertices)
-        self.V[:, self.genderIndex] = Util.randomChoice(numpy.array([1, 1]), numVertices)
+        self.V = self.__generateRandomVertices(numVertices)
+
+
+    def __generateRandomVertices(self, n): 
+        V = numpy.zeros((n, self.numFeatures))
+        V[:, self.dobIndex] = numpy.random.rand(n)
+        V[:, self.genderIndex] = Util.randomChoice(numpy.array([1, 1]), n)
         #Note in reality females cannot be recorded as bisexual but we model the real scenario
         #We assume that 5% of the population is gay or bisexual 
-        self.V[:, self.orientationIndex] = Util.randomChoice(numpy.array([19, 1]), numVertices)
+        V[:, self.orientationIndex] = Util.randomChoice(numpy.array([19, 1]), n)
 
-        self.V[:, self.stateIndex] = numpy.zeros(numVertices)
-        self.V[:, self.infectionTimeIndex] = numpy.ones(numVertices)*-1
-        self.V[:, self.detectionTimeIndex] = numpy.ones(numVertices)*-1
-        self.V[:, self.detectionTypeIndex] = numpy.ones(numVertices)*-1
+        V[:, self.stateIndex] = numpy.zeros(n)
+        V[:, self.infectionTimeIndex] = numpy.ones(n)*-1
+        V[:, self.detectionTimeIndex] = numpy.ones(n)*-1
+        V[:, self.detectionTypeIndex] = numpy.ones(n)*-1
 
-        self.V[:, self.hiddenDegreeIndex] = numpy.ones(numVertices)*-1
+        V[:, self.hiddenDegreeIndex] = numpy.ones(n)*-1
+        
+        return V 
+        
+    def addVertices(self, n):
+        """
+        We add n vertices to this object, randomly generating their features 
+        """
+        super(HIVVertices, self).addVertices(n)
+        self.V[-n:, :] = self.__generateRandomVertices(n)
 
     def setInfected(self, vertexInd, time):
         Parameter.checkIndex(vertexInd, 0, self.getNumVertices())
@@ -56,6 +70,16 @@ class HIVVertices(VertexList):
         vList = HIVVertices(self.V.shape[0])
         vList.setVertices(numpy.copy(self.V))
         return vList
+
+    def subList(self, indices):
+        """
+        Returns a subset of this object, indicated by the given indices.
+        """
+        Parameter.checkList(indices, Parameter.checkIndex, (0, self.getNumVertices()))
+        vList = HIVVertices(len(indices))
+        vList.setVertices(self.getVertices(indices))
+
+        return vList 
 
     #Some static variables
     dobIndex = 0
