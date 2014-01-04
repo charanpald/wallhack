e3ec9a9901034c433bf50f531ecc81da5c95082b
Charanpal Dhanjal <charanpal@gmail.com>
2014-01-04 17:48:44 +0000

    Add bit on variable importance

--- wallhack/metabolomics/ProcessResults.py
+++ wallhack/metabolomics/ProcessResults.py
@@ -1,13 +1,17 @@
 import sys
 import numpy 
 import logging
-import datetime 
+import datetime
+import matplotlib 
+matplotlib.use("GTK3Agg")
+import matplotlib.pyplot as plt  
 from apgl.util.PathDefaults import PathDefaults
 from apgl.util.Latex import Latex 
 from wallhack.metabolomics.MetabolomicsUtils import MetabolomicsUtils
 from wallhack.metabolomics.MetabolomicsExpHelper import MetabolomicsExpHelper
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+numpy.set_printoptions(suppress=True, precision=3)
 resultsDir = PathDefaults.getOutputDir() + "metabolomics/"
 figureDir = resultsDir + "Figures/"
 
@@ -62,3 +66,43 @@ for i, dataName in enumerate(dataNames):
     table = Latex.array2DToRows(testAucsMean[:, i, :].T, testAucsStd[:, i, :].T, precision=2, bold=maxAUCs)
     print(Latex.listToRow(hormoneNameIndicators))
     print(Latex.addRowNames(algorithms, table))
+    
+
+#Now looks at the features for the raw spectra 
+algorithm = "L1SvmTreeRankForest" 
+dataName = "raw"
+numMissingFiles = 0 
+numFeatures = 100
+
+numIndicators = 6 
+featureInds = numpy.zeros((numFeatures, numIndicators))
+
+for i, (hormoneName, hormoneConc) in enumerate(helper.hormoneDict.items()):
+    try: 
+        fileName = resultsDir + "Weights" + algorithm + "-" + hormoneName + "-0" + "-" + dataName + ".npy"
+        weights = numpy.load(fileName)
+        weights = weights/numpy.linalg.norm(weights)
+        
+        inds0 = numpy.flipud(numpy.argsort(weights))[0:numFeatures]
+
+        #print(inds0 == 950)
+        featureInds[:, 2*i] = inds0
+    except IOError: 
+        logging.debug("File not found : " + str(fileName))
+        numMissingFiles += 1 
+        
+    try: 
+        fileName = resultsDir + "Weights" + algorithm + "-" + hormoneName + "-2" + "-" + dataName + ".npy"
+        weights = numpy.load(fileName)
+        weights = weights/numpy.linalg.norm(weights)
+        
+        inds2 = numpy.flipud(numpy.argsort(weights))[0:numFeatures]
+        #print(inds2 == 950)
+        featureInds[:, 2*i+1] = inds2
+    except IOError: 
+        logging.debug("File not found : " + str(fileName))
+        numMissingFiles += 1 
+        
+    print(numpy.intersect1d(inds0, inds2).shape)
+        
+print(Latex.array2DToRows(featureInds, precision=0))
