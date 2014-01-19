c1bc2e5bf4b9c0b6e0f56958e7712ffb998bd434
Charanpal Dhanjal <charanpal@gmail.com>
2014-01-19 15:50:32 +0100

    Plot AUC and objective

--- wallhack/recommendexp/MendeleyCoauthorsExp.py
+++ wallhack/recommendexp/MendeleyCoauthorsExp.py
@@ -7,16 +7,17 @@ from sandbox.util.SparseUtils import SparseUtils
 import matplotlib 
 matplotlib.use("GTK3Agg")
 import matplotlib.pyplot as plt 
+from apgl.util.ProfileUtils import ProfileUtils
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 numpy.random.seed(21)        
 numpy.set_printoptions(precision=3, suppress=True, linewidth=150)
 
 #Create a low rank matrix  
-m = 50 
-n = 20 
+m = 500 
+n = 100 
 k = 5 
-numInds = 500
+numInds = 10000
 X = SparseUtils.generateSparseLowRank((m, n), k, numInds)
 
 X = X/X
@@ -24,23 +25,38 @@ X = X.tocsr()
 
 lmbda = 0.00001
 r = numpy.ones(X.shape[0])*0.0
-eps = 0.02
-sigma = 100
-maxLocalAuc = MaxLocalAUC(lmbda, k, r, sigma=sigma, eps=eps, stochastic=False)
+eps = 0.5
+sigma = 200
+stochastic = True
+maxLocalAuc = MaxLocalAUC(lmbda, k, r, sigma=sigma, eps=eps, stochastic=stochastic)
+maxLocalAuc.maxIterations = 100
+maxLocalAuc.numSamples = 100
+maxLocalAuc.approxDerivative = False
         
 omegaList = maxLocalAuc.getOmegaList(X)
 startTime = time.time()
-U, V, objs = maxLocalAuc.learnModel(X, True)
+
+logging.debug("Starting training")
+ProfileUtils.profile('U, V, objs, aucs, iterations = maxLocalAuc.learnModel(X, True)', globals(), locals())
+#U, V, objs, aucs, iterations = maxLocalAuc.learnModel(X, True)
 totalTime = time.time() - startTime 
 
-print(numpy.linalg.norm(U), numpy.linalg.norm(V))
+logging.debug("||U||=" + str(numpy.linalg.norm(U)) + " ||V||=" + str(numpy.linalg.norm(V)))
 logging.debug("Final local AUC:" + str(maxLocalAuc.localAUC(X, U, V, omegaList)))
 logging.debug("Total time taken: " + str(totalTime))
-logging.debug("Number of iterations: " + str(len(objs)))
+logging.debug("Number of iterations: " + str(iterations))
 
-print(U)
-print(V)
+#print(U)
+#print(V)
 #print(objs)
 
+plt.figure(0)
 plt.plot(objs)
+plt.xlabel("iteration")
+plt.ylabel("objective")
+
+plt.figure(1)
+plt.plot(aucs)
+plt.xlabel("iteration")
+plt.ylabel("local AUC")
 plt.show()
