b1c2fd4cd3e7e332f6c35f6a0c93dd42f1965b0b
Charanpal Dhanjal <charanpal@gmail.com>
2014-01-21 13:28:49 +0100

    File rename

--- wallhack/recommendexp/SyntheticRankingExp1.py
+++ wallhack/recommendexp/SyntheticRankingExp1.py
@@ -2,6 +2,7 @@ import numpy
 import logging
 import sys
 import time
+import sppy 
 from sandbox.recommendation.MaxLocalAUC import MaxLocalAUC
 from sandbox.util.SparseUtils import SparseUtils
 import matplotlib 
@@ -15,7 +16,7 @@ numpy.set_printoptions(precision=3, suppress=True, linewidth=150)
 
 #Create a low rank matrix  
 m = 1000 
-n = 500 
+n = 2000 
 k = 10 
 numInds = 50000
 X = SparseUtils.generateSparseLowRank((m, n), k, numInds)
@@ -23,35 +24,30 @@ X = SparseUtils.generateSparseLowRank((m, n), k, numInds)
 X = X/X
 X = X.tocsr()
 
+
 lmbda = 0.00001
 r = numpy.ones(X.shape[0])*0.0
-eps = 0.01
+eps = 0.0000001
 sigma = 200
 stochastic = True
 maxLocalAuc = MaxLocalAUC(lmbda, k, r, sigma=sigma, eps=eps, stochastic=stochastic)
-maxLocalAuc.maxIterations = 200
-maxLocalAuc.numRowSamples = 100
+maxLocalAuc.maxIterations = 500
+maxLocalAuc.numRowSamples = 50
 maxLocalAuc.numAucSamples = 200
 maxLocalAuc.approxDerivative = True
 maxLocalAuc.initialAlg = "svd"
 maxLocalAuc.recordStep = 20
         
 omegaList = maxLocalAuc.getOmegaList(X)
-startTime = time.time()
 
 logging.debug("Starting training")
 ProfileUtils.profile('U, V, objs, aucs, iterations = maxLocalAuc.learnModel(X, True)', globals(), locals())
 #U, V, objs, aucs, iterations = maxLocalAuc.learnModel(X, True)
-totalTime = time.time() - startTime 
 
 logging.debug("||U||=" + str(numpy.linalg.norm(U)) + " ||V||=" + str(numpy.linalg.norm(V)))
 logging.debug("Final local AUC:" + str(maxLocalAuc.localAUCApprox(X, U, V, omegaList)))
-logging.debug("Total time taken: " + str(totalTime))
-logging.debug("Number of iterations: " + str(iterations))
 
-#print(U)
-#print(V)
-#print(objs)
+logging.debug("Number of iterations: " + str(iterations))
 
 plt.figure(0)
 plt.plot(objs)
