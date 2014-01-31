c8db1e41950e4e9cda3af85f53c72ca6ab25f026
Charanpal Dhanjal <charanpal@gmail.com>
2014-01-31 17:41:22 +0100

    Update for quantile

--- wallhack/recommendexp/SyntheticRankingExp1.py
+++ wallhack/recommendexp/SyntheticRankingExp1.py
@@ -13,10 +13,11 @@ numpy.random.seed(21)
 numpy.set_printoptions(precision=3, suppress=True, linewidth=150)
 
 #Create a low rank matrix  
-m = 2000 
-n = 2000 
+density = 0.05
+m = 1000 
+n = 1000 
 k = 10 
-numInds = int(m*n*0.01)
+numInds = int(m*n*density)
 X = SparseUtils.generateSparseLowRank((m, n), k, numInds)
 
 X = X/X
@@ -24,27 +25,29 @@ X = X.tocsr()
 
 
 lmbda = 0.000
-r = numpy.ones(X.shape[0])*0.0
+u = 0.3
 eps = 0.001
 sigma = 0.1
 stochastic = True
-maxLocalAuc = MaxLocalAUC(lmbda, k, r, sigma=sigma, eps=eps, stochastic=stochastic)
-maxLocalAuc.maxIterations = 1000
+maxLocalAuc = MaxLocalAUC(lmbda, k, u, sigma=sigma, eps=eps, stochastic=stochastic)
+maxLocalAuc.maxIterations = m*2
 maxLocalAuc.numRowSamples = 50
-maxLocalAuc.numAucSamples = 20
-maxLocalAuc.iterationsPerUpdate = 50
+maxLocalAuc.numColSamples = 20
+maxLocalAuc.numAucSamples = 30
 maxLocalAuc.initialAlg = "rand"
 maxLocalAuc.recordStep = 5
-maxLocalAuc.rate = "constant"
-        
+maxLocalAuc.rate = "optimal"
+maxLocalAuc.alpha = 0.1    
+maxLocalAuc.t0 = 0.1    
 omegaList = maxLocalAuc.getOmegaList(X)
 
 logging.debug("Starting training")
 ProfileUtils.profile('U, V, objs, aucs, iterations, time = maxLocalAuc.learnModel(X, True)', globals(), locals())
 #U, V, objs, aucs, iterations, times = maxLocalAuc.learnModel(X, True)
 
+r = maxLocalAuc.computeR(U, V)
 logging.debug("||U||=" + str(numpy.linalg.norm(U)) + " ||V||=" + str(numpy.linalg.norm(V)))
-logging.debug("Final local AUC:" + str(maxLocalAuc.localAUCApprox(X, U, V, omegaList)))
+logging.debug("Final local AUC:" + str(maxLocalAuc.localAUCApprox(X, U, V, omegaList, r)))
 
 logging.debug("Number of iterations: " + str(iterations))
 
