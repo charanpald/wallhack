4815f479b82c416e6f0bb4deca6ed8db4789fc14
Charanpal Dhanjal <charanpal@gmail.com>
2014-04-09 15:22:04 +0200

    Print ROC

--- wallhack/rankingexp/SyntheticRankingExp1.py
+++ wallhack/rankingexp/SyntheticRankingExp1.py
@@ -35,39 +35,48 @@ logging.debug("Number of non-zero elements: " + str((trainX.nnz, testX.nnz)))
 
 #w = 1.0
 k2 = k
-eps = 10**-4
+eps = 10**-5
 sigma = 10
-lmbda = 0.02
+lmbda = 0.01
 maxLocalAuc = MaxLocalAUC(k2, w, sigma=sigma, eps=eps, lmbda=lmbda, stochastic=True)
 maxLocalAuc.maxIterations = m*20
 maxLocalAuc.numRowSamples = 20
 maxLocalAuc.numStepIterations = 200
 maxLocalAuc.numAucSamples = 50
-maxLocalAuc.initialAlg = "rand"
+maxLocalAuc.initialAlg = "svd"
 maxLocalAuc.recordStep = maxLocalAuc.numStepIterations
 maxLocalAuc.nu = 20
 maxLocalAuc.rate = "optimal"
-maxLocalAuc.alpha = 2.0
+maxLocalAuc.alpha = 0.3
 maxLocalAuc.t0 = 10**-3
 maxLocalAuc.folds = 3
-maxLocalAuc.rho = 0
+maxLocalAuc.rho = 0.00
+maxLocalAuc.ks = 2**numpy.arange(3, 7)
 
 logging.debug("Starting training")
 logging.debug(maxLocalAuc)
-#maxLocalAuc.modelSelect(trainX)
+maxLocalAuc.modelSelect(trainX)
 #ProfileUtils.profile('U, V, trainObjs, trainAucs, testObjs, testAucs, iterations, time = maxLocalAuc.learnModel(trainX, testX=X, verbose=True)', globals(), locals())
 U, V, trainObjs, trainAucs, testObjs, testAucs, iterations, time = maxLocalAuc.learnModel(trainX, testX=X, verbose=True)
 
+fpr, tpr = MCEvaluator.averageRocCurve(X, U, V)
 
 plt.figure(0)
 plt.plot(trainObjs, label="train")
 plt.plot(testObjs, label="test")
 plt.xlabel("iteration")
 plt.ylabel("objective")
+plt.legend()
 
 plt.figure(1)
 plt.plot(trainAucs, label="train")
 plt.plot(testAucs, label="test")
 plt.xlabel("iteration")
 plt.ylabel("local AUC")
+plt.legend()
+
+plt.figure(2)
+plt.plot(fpr, tpr)
+plt.xlabel("mean false positive rate")
+plt.ylabel("mean true positive rate")
 plt.show()
