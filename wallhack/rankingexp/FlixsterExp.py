485a0451fd4177f5f10939d25ae65f9e0eefffbc
Charanpal Dhanjal <charanpal@gmail.com>
2014-04-15 14:35:49 +0200

    First commit

---
+++ wallhack/rankingexp/FlixsterExp.py
@@ -0,0 +1,98 @@
+import numpy
+import logging
+import sys
+import argparse 
+import os
+import errno
+import sppy 
+import array
+from wallhack.rankingexp.RankingExpHelper import RankingExpHelper
+from sandbox.util.PathDefaults import PathDefaults
+from sandbox.util.IdIndexer import IdIndexer 
+from sandbox.util.SparseUtils import SparseUtils 
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+numpy.random.seed(21)        
+numpy.set_printoptions(precision=3, suppress=True, linewidth=150)
+
+#Create a low rank matrix  
+matrixFileName = PathDefaults.getDataDir() + "flixster/Ratings.timed.txt" 
+
+print(matrixFileName)
+matrixFile = open(matrixFileName)
+matrixFile.readline()
+userIndexer = IdIndexer("i")
+movieIndexer = IdIndexer("i")
+
+ratings = array.array("f")
+
+for i, line in enumerate(matrixFile):
+    if i % 1000000 == 0: 
+        print("Iteration: " + str(i))
+    vals = line.split()
+    
+    userIndexer.append(vals[0])
+    movieIndexer.append(vals[1])
+    ratings.append(float(vals[2]))
+
+rowInds = userIndexer.getArray()
+colInds = movieIndexer.getArray()
+ratings = numpy.array(ratings)
+print(rowInds.dtype)
+print(colInds.dtype)
+print(ratings.shape, ratings.dtype)
+
+X = sppy.csarray((len(userIndexer.getIdDict()), len(movieIndexer.getIdDict())), storagetype="row", dtype=numpy.int)
+X.put(numpy.array(ratings>3, numpy.int), numpy.array(rowInds, numpy.int32), numpy.array(colInds, numpy.int32), init=True)
+X = SparseUtils.pruneMatrix(X, minNnzRows=10, minNnzCols=10)
+logging.debug("Read file: " + matrixFileName)
+logging.debug("Shape of data: " + str(X.shape))
+logging.debug("Number of non zeros " + str(X.nnz))
+
+(m, n) = X.shape
+
+# Arguments related to the dataset
+dataArgs = argparse.Namespace()
+
+# Arguments related to the algorithm
+defaultAlgoArgs = argparse.Namespace()
+defaultAlgoArgs.ks = 2**numpy.arange(3, 7)
+defaultAlgoArgs.rhos = numpy.flipud(numpy.logspace(-7, -3, 5))
+defaultAlgoArgs.folds = 4
+defaultAlgoArgs.u = 20.0/m
+defaultAlgoArgs.maxIterations = 10*m
+defaultAlgoArgs.t0 = 10**-3
+defaultAlgoArgs.alpha = 0.2
+defaultAlgoArgs.initialAlg = "softimpute"
+
+# data args parser #
+dataParser = argparse.ArgumentParser(description="", add_help=False)
+dataParser.add_argument("-h", "--help", action="store_true", help="show this help message and exit")
+devNull, remainingArgs = dataParser.parse_known_args(namespace=dataArgs)
+if dataArgs.help:
+    helpParser  = argparse.ArgumentParser(description="", add_help=False, parents=[dataParser, RankingExpHelper.newAlgoParser(defaultAlgoArgs)])
+    helpParser.print_help()
+    exit()
+
+dataArgs.extendedDirName = ""
+dataArgs.extendedDirName += "MovieLens"
+
+# print args #
+logging.info("Running on " + dataArgs.extendedDirName)
+logging.info("Data params:")
+keys = list(vars(dataArgs).keys())
+keys.sort()
+for key in keys:
+    logging.info("    " + str(key) + ": " + str(dataArgs.__getattribute__(key)))
+
+logging.info("Creating the exp-runner")
+rankingExpHelper = RankingExpHelper(remainingArgs, defaultAlgoArgs, dataArgs.extendedDirName)
+rankingExpHelper.printAlgoArgs()
+#    os.makedirs(resultsDir, exist_ok=True) # for python 3.2
+try:
+    os.makedirs(rankingExpHelper.resultsDir)
+except OSError as err:
+    if err.errno != errno.EEXIST:
+        raise
+
+rankingExpHelper.runExperiment(X)
