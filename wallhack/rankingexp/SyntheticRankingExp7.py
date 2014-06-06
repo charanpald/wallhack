be12554039c664dc617bb9b85d1b26599e3cca8b
Charanpal Dhanjal <charanpal@gmail.com>
2014-06-06 14:55:16 +0100

    Test rho and u

--- wallhack/rankingexp/SyntheticRankingExp7.py
+++ wallhack/rankingexp/SyntheticRankingExp7.py
@@ -1,99 +1,124 @@
-
-
 import numpy
 import logging
 import sys
-import sppy
 from sandbox.recommendation.MaxLocalAUC import MaxLocalAUC
 from sandbox.util.SparseUtils import SparseUtils
 from sandbox.util.PathDefaults import PathDefaults
 from sandbox.util.MCEvaluator import MCEvaluator
 from sandbox.util.MCEvaluatorCython import MCEvaluatorCython
 from sandbox.util.Sampling import Sampling
-import matplotlib 
-matplotlib.use("GTK3Agg")
-import matplotlib.pyplot as plt
+from wallhack.rankingexp.DatasetUtils import DatasetUtils
 
 """
 Test the effect of  quantile threshold u. 
 """
 
-
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 #numpy.random.seed(22)        
 #numpy.set_printoptions(precision=3, suppress=True, linewidth=150)
 
-m = 500
-n = 200
-k = 8 
-u = 20.0/n
-w = 1-u
-X, U, s, V = SparseUtils.generateSparseBinaryMatrix((m,n), k, w, csarray=True, verbose=True, indsPerRow=200)
-logging.debug("Number of non zero elements: " + str(X.nnz))
-logging.debug("Size of X: " + str(X.shape))
+if len(sys.argv) > 1:
+    dataset = sys.argv[1]
+else: 
+    dataset = "synthetic"
 
-U = U*s
+saveResults = True
 
+if dataset == "synthetic": 
+    X, U, V = DatasetUtils.syntheticDataset1()
+    outputFile = PathDefaults.getOutputDir() + "ranking/Exp7SyntheticResults.npz" 
+elif dataset == "movielens": 
+    X = DatasetUtils.movieLens()
+    outputFile = PathDefaults.getOutputDir() + "ranking/Exp7MovieLensResults.npz" 
+elif dataset == "flixster": 
+    X = DatasetUtils.flixster()
+    outputFile = PathDefaults.getOutputDir() + "ranking/Exp7FlixsterResults.npz" 
+    X = X[0:1000, :]
+else: 
+    raise ValueError("Unknown dataset: " + dataset)
+        
 testSize = 5
-trainTestXs = Sampling.shuffleSplitRows(X, 1, testSize)
-trainX, testX = trainTestXs[0]
+folds = 3
+trainTestXs = Sampling.shuffleSplitRows(X, folds, testSize)
 
-trainOmegaPtr = SparseUtils.getOmegaListPtr(trainX)
-testOmegaPtr = SparseUtils.getOmegaListPtr(testX)
-allOmegaPtr = SparseUtils.getOmegaListPtr(X)
-
-logging.debug("Number of non-zero elements: " + str((trainX.nnz, testX.nnz)))
-
-#logging.debug("Total local AUC:" + str(MCEvaluator.localAUC(X, U, V, w)))
-#logging.debug("Train local AUC:" + str(MCEvaluator.localAUC(trainX, U, V, w)))
-#logging.debug("Test local AUC:" + str(MCEvaluator.localAUC(testX, U, V, w)))
-
-#w = 1.0
-u2 = 0.05 
-w2 = 1-u2 
-k2 = k
+u = 0.1 
+w2 = 1-u 
+k = 8
 eps = 10**-6
-maxLocalAuc = MaxLocalAUC(k2, w2, eps=eps, stochastic=True)
+maxLocalAuc = MaxLocalAUC(k, w2, eps=eps, stochastic=True)
 maxLocalAuc.maxIterations = 50
-maxLocalAuc.numRowSamples = 100
+maxLocalAuc.numRowSamples = 10
 maxLocalAuc.numAucSamples = 10
-maxLocalAuc.initialAlg = "rand"
-maxLocalAuc.recordStep = 2000
+maxLocalAuc.initialAlg = "svd"
+maxLocalAuc.recordStep = 5
 maxLocalAuc.rate = "optimal"
 maxLocalAuc.alpha = 0.5
-maxLocalAuc.t0 = 10**-4
-maxLocalAuc.lmbda = 0.001
+maxLocalAuc.t0 = 10**-2
+maxLocalAuc.lmbda = 0.01
 maxLocalAuc.rho = 1.0
 
 numRecordAucSamples = 200
-trainOmegaList = SparseUtils.getOmegaList(trainX)
-testOmegaList = SparseUtils.getOmegaList(testX)
-
 maxItems = 10
-us = numpy.array([0.5, 0.2, 0.1, 0.05, 0.01])
+us = numpy.linspace(0, 1, 10)
 
-trainLocalAucs = numpy.zeros(us.shape[0])
-trainPrecisions = numpy.zeros(us.shape[0])
-trainRecalls = numpy.zeros(us.shape[0])
-
-testLocalAucs = numpy.zeros(us.shape[0])
-testPrecisions = numpy.zeros(us.shape[0])
-testRecalls = numpy.zeros(us.shape[0])
-
-for i, u in enumerate(us): 
-    maxLocalAuc.w = 1-u
-    logging.debug(maxLocalAuc)
-    U, V, trainObjs, trainAucs, testObjs, testAucs, ind, totalTime = maxLocalAuc.learnModel(trainX, verbose=True, testX=testX)
+if saveResults: 
+    trainLocalAucs = numpy.zeros(us.shape[0])
+    trainPrecisions = numpy.zeros(us.shape[0])
+    trainRecalls = numpy.zeros(us.shape[0])
     
-    trainLocalAucs[i] = trainAucs[-1]
-    trainOrderedItems = MCEvaluator.recommendAtk(U, V, maxItems)    
-    trainPrecisions[i] = MCEvaluator.precisionAtK(trainOmegaPtr, trainOrderedItems, maxItems)
-    trainRecalls[i] = MCEvaluator.recallAtK(trainX, trainOrderedItems, maxItems)
+    testLocalAucs = numpy.zeros(us.shape[0])
+    testPrecisions = numpy.zeros(us.shape[0])
+    testRecalls = numpy.zeros(us.shape[0])
     
-    testLocalAucs[i] = testAucs[-1]
-    testOrderedItems = MCEvaluatorCython.recommendAtk(U, V, maxItems, trainX)
-    testPrecisions[i] = MCEvaluator.precisionAtK(testX, testOrderedItems, maxItems)
-    testRecalls[i] = MCEvaluator.recallAtK(testX, testOrderedItems, maxItems)
+    for trainX, testX in trainTestXs: 
+        trainOmegaPtr = SparseUtils.getOmegaListPtr(trainX)
+        testOmegaPtr = SparseUtils.getOmegaListPtr(testX)
+        allOmegaPtr = SparseUtils.getOmegaListPtr(X)
+        logging.debug("Number of non-zero elements: " + str((trainX.nnz, testX.nnz)))        
+        
+        for i, u in enumerate(us): 
+            maxLocalAuc.w = 1-u
+            logging.debug(maxLocalAuc)
+            U, V, trainObjs, trainAucs, testObjs, testAucs, ind, totalTime = maxLocalAuc.learnModel(trainX, verbose=True)
+            
+            trainLocalAucs[i] += trainAucs[-1]
+            trainOrderedItems = MCEvaluator.recommendAtk(U, V, maxItems)    
+            trainPrecisions[i] += MCEvaluator.precisionAtK(trainOmegaPtr, trainOrderedItems, maxItems)
+            trainRecalls[i] += MCEvaluator.recallAtK(trainX, trainOrderedItems, maxItems)
+            
+            testLocalAucs[i] += testAucs[-1]
+            testOrderedItems = MCEvaluatorCython.recommendAtk(U, V, maxItems, trainX)
+            testPrecisions[i] += MCEvaluator.precisionAtK(testX, testOrderedItems, maxItems)
+            testRecalls[i] += MCEvaluator.recallAtK(testX, testOrderedItems, maxItems)
+        
+    testLocalAucs /= folds 
+    testPrecisions /= folds 
+    testRecalls /= folds 
+    
+    numpy.savez(outputFile, testLocalAucs, testPrecisions, testRecalls)
+else: 
+    data = numpy.load(outputFile)
+    testLocalAucs, testPrecisions, testRecalls = data["arr_0"], data["arr_1"], data["arr_2"]
+    import matplotlib 
+    matplotlib.use("GTK3Agg")
+    import matplotlib.pyplot as plt 
+    
+    plt.figure(0)
+    plt.plot(us, testLocalAucs)
+    plt.xlabel("u")
+    plt.ylabel("local AUC")
+    
+    plt.figure(1)
+    plt.plot(us, testPrecisions)
+    plt.xlabel("u")
+    plt.ylabel("precision")
+    
+    plt.figure(2)
+    plt.plot(us, testRecalls)
+    plt.xlabel("u")
+    plt.ylabel("recall")
+    
+    plt.show()
 
 print(trainLocalAucs)
 print(trainPrecisions)
--- wallhack/rankingexp/SyntheticRankingExp9.py
+++ wallhack/rankingexp/SyntheticRankingExp9.py
@@ -1,96 +1,141 @@
-
-
 import numpy
 import logging
 import sys
 import sppy
-import os 
-import multiprocessing 
 from sandbox.recommendation.MaxLocalAUC import MaxLocalAUC
 from sandbox.util.SparseUtils import SparseUtils
 from sandbox.util.PathDefaults import PathDefaults
 from sandbox.util.MCEvaluator import MCEvaluator
 from sandbox.util.MCEvaluatorCython import MCEvaluatorCython
 from sandbox.util.Sampling import Sampling
-import matplotlib 
-matplotlib.use("GTK3Agg")
-import matplotlib.pyplot as plt
+from wallhack.rankingexp.DatasetUtils import DatasetUtils
 
 """
-See how regularisation can help overfitting 
+Test the effect of varying rho. 
 """
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 #numpy.random.seed(22)        
 #numpy.set_printoptions(precision=3, suppress=True, linewidth=150)
 
-os.system('taskset -p 0xffffffff %d' % os.getpid())
+if len(sys.argv) > 1:
+    dataset = sys.argv[1]
+else: 
+    dataset = "synthetic"
 
-def computeTestAuc(args): 
-    trainX, testX, maxLocalAuc  = args 
-    
-    U, V, trainObjs, trainAucs, testObjs, testAucs, iterations, totalTime = maxLocalAuc.learnModel(trainX, testX=testX, verbose=True)
-    trainLocalAuc = trainAucs[-1]
-    testLocalAuc = testAucs[-1]
-        
-    return trainLocalAuc, testLocalAuc 
+saveResults = True
 
-
-m = 500
-n = 200
-k = 8 
-u = 20.0/n
-w = 1-u
-X, U, s, V = SparseUtils.generateSparseBinaryMatrix((m,n), k, w, csarray=True, verbose=True, indsPerRow=200)
-logging.debug("Number of non zero elements: " + str(X.nnz))
-logging.debug("Size of X: " + str(X.shape))
-
-U = U*s
+if dataset == "synthetic": 
+    X, U, V = DatasetUtils.syntheticDataset1()
+    outputFile = PathDefaults.getOutputDir() + "ranking/Exp9SyntheticResults.npz" 
+elif dataset == "movielens": 
+    X = DatasetUtils.movieLens()
+    outputFile = PathDefaults.getOutputDir() + "ranking/Exp9MovieLensResults.npz" 
+elif dataset == "flixster": 
+    X = DatasetUtils.flixster()
+    outputFile = PathDefaults.getOutputDir() + "ranking/Exp9FlixsterResults.npz" 
+    X = X[0:1000, :]
+else: 
+    raise ValueError("Unknown dataset: " + dataset)
 
 testSize = 5
-trainTestXs = Sampling.shuffleSplitRows(X, 1, testSize)
+folds = 3
+trainTestXs = Sampling.shuffleSplitRows(X, folds, testSize)
 trainX, testX = trainTestXs[0]
+
+trainOmegaPtr = SparseUtils.getOmegaListPtr(trainX)
+testOmegaPtr = SparseUtils.getOmegaListPtr(testX)
+allOmegaPtr = SparseUtils.getOmegaListPtr(X)
 
 logging.debug("Number of non-zero elements: " + str((trainX.nnz, testX.nnz)))
 
+u = 0.1 
+w2 = 1-u 
+k = 8
 eps = 10**-6
-maxLocalAuc = MaxLocalAUC(k, w, eps=eps, stochastic=True)
+maxLocalAuc = MaxLocalAUC(k, w2, eps=eps, stochastic=True)
 maxLocalAuc.maxIterations = 50
-maxLocalAuc.numRowSamples = 100
+maxLocalAuc.numRowSamples = 10
 maxLocalAuc.numAucSamples = 10
-maxLocalAuc.initialAlg = "rand"
-maxLocalAuc.recordStep = m
-maxLocalAuc.rho = 1.0
+maxLocalAuc.initialAlg = "svd"
+maxLocalAuc.recordStep = 5
 maxLocalAuc.rate = "optimal"
 maxLocalAuc.alpha = 0.5
-maxLocalAuc.t0 = 10**-3
-maxLocalAuc.lmbda = 0.0001
+maxLocalAuc.t0 = 10**-2
+maxLocalAuc.lmbda = 0.01
+maxLocalAuc.rho = 1.0
 
+numRecordAucSamples = 200
+trainOmegaList = SparseUtils.getOmegaList(trainX)
+testOmegaList = SparseUtils.getOmegaList(testX)
 
-maxItems = 20
-lmbdas = 2.0**-numpy.arange(1, 14, 2)
+maxItems = 10
+rhos = numpy.linspace(0, 1, 10)
 
-trainLocalAucs = numpy.zeros(lmbdas.shape[0])
-testLocalAucs = numpy.zeros(lmbdas.shape[0])
-
-paramList = [] 
-
-for i, lmbda in enumerate(lmbdas):  
-    maxLocalAuc.lmbda = lmbda
-    logging.debug(maxLocalAuc)
+if saveResults: 
+    trainLocalAucs = numpy.zeros(rhos.shape[0])
+    trainPrecisions = numpy.zeros(rhos.shape[0])
+    trainRecalls = numpy.zeros(rhos.shape[0])
     
-    paramList.append((trainX, testX, maxLocalAuc.copy()))
+    testLocalAucs = numpy.zeros(rhos.shape[0])
+    testPrecisions = numpy.zeros(rhos.shape[0])
+    testRecalls = numpy.zeros(rhos.shape[0])
+    
+    for trainX, testX in trainTestXs: 
+        trainOmegaPtr = SparseUtils.getOmegaListPtr(trainX)
+        testOmegaPtr = SparseUtils.getOmegaListPtr(testX)
+        allOmegaPtr = SparseUtils.getOmegaListPtr(X)
+        logging.debug("Number of non-zero elements: " + str((trainX.nnz, testX.nnz)))        
+    
+        for i, rho in enumerate(rhos): 
+            maxLocalAuc.rho = rho
+            logging.debug(maxLocalAuc)
+            U, V, trainObjs, trainAucs, testObjs, testAucs, ind, totalTime = maxLocalAuc.learnModel(trainX, verbose=True)
+            
+            trainLocalAucs[i] += trainAucs[-1]
+            trainOrderedItems = MCEvaluator.recommendAtk(U, V, maxItems)    
+            trainPrecisions[i] += MCEvaluator.precisionAtK(trainOmegaPtr, trainOrderedItems, maxItems)
+            trainRecalls[i] += MCEvaluator.recallAtK(trainX, trainOrderedItems, maxItems)
+            
+            testLocalAucs[i] += testAucs[-1]
+            testOrderedItems = MCEvaluatorCython.recommendAtk(U, V, maxItems, trainX)
+            testPrecisions[i] += MCEvaluator.precisionAtK(testX, testOrderedItems, maxItems)
+            testRecalls[i] += MCEvaluator.recallAtK(testX, testOrderedItems, maxItems)
+        
+    testLocalAucs /= folds 
+    testPrecisions /= folds 
+    testRecalls /= folds         
+        
+    numpy.savez(outputFile, testLocalAucs, testPrecisions, testRecalls)
+else: 
+    data = numpy.load(outputFile)
+    testLocalAucs, testPrecisions, testRecalls = data["arr_0"], data["arr_1"], data["arr_2"]
+    import matplotlib 
+    matplotlib.use("GTK3Agg")
+    import matplotlib.pyplot as plt 
+    
+    plt.figure(0)
+    plt.plot(rhos, testLocalAucs)
+    plt.xlabel("rho")
+    plt.ylabel("local AUC")
+    
+    plt.figure(1)
+    plt.plot(rhos, testPrecisions)
+    plt.xlabel("rho")
+    plt.ylabel("precision")
+    
+    plt.figure(2)
+    plt.plot(rhos, testRecalls)
+    plt.xlabel("rho")
+    plt.ylabel("recall")
+    
+    plt.show()
 
-pool = multiprocessing.Pool(processes=7, maxtasksperchild=100)
-resultsIterator = pool.imap(computeTestAuc, paramList, 1)
-       
-
-for i, lmbda in enumerate(lmbdas): 
-    trainLocalAucs[i], testLocalAucs[i] = resultsIterator.next()
-
-pool.terminate()
-      
-print(lmbdas)      
 print(trainLocalAucs)
-print(testLocalAucs)
+print(trainPrecisions)
+print(trainRecalls)
+print("\n")
 
+print(testLocalAucs)
+print(testPrecisions)
+print(testRecalls)
