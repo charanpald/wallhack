5dd35a38f16b77ca9195040ec15a86cd5b2f5e6d
Charanpal Dhanjal <charanpal@gmail.com>
2014-06-06 14:57:00 +0100

    Include flixster in datasetutils

--- wallhack/rankingexp/DatasetUtils.py
+++ wallhack/rankingexp/DatasetUtils.py
@@ -1,8 +1,10 @@
 import numpy 
 import logging
 import sppy 
+import array 
 from sandbox.util.SparseUtils import SparseUtils
 from sandbox.util.PathDefaults import PathDefaults 
+from sandbox.util.IdIndexer import IdIndexer 
 
 class DatasetUtils(object): 
     """
@@ -32,6 +34,38 @@ class DatasetUtils(object):
         X = SparseUtils.pruneMatrixRows(X, minNnzRows=10)
         logging.debug("Read file: " + matrixFileName)
         logging.debug("Non zero elements: " + str(X.nnz) + " shape: " + str(X.shape))
-        (m, n) = X.shape
-        
+
         return X
+        
+    @staticmethod 
+    def flixster(): 
+        matrixFileName = PathDefaults.getDataDir() + "flixster/Ratings.timed.txt" 
+        matrixFile = open(matrixFileName)
+        matrixFile.readline()
+        userIndexer = IdIndexer("i")
+        movieIndexer = IdIndexer("i")
+        
+        ratings = array.array("f")
+        logging.debug("Loading ratings from " + matrixFileName)
+        
+        for i, line in enumerate(matrixFile):
+            if i % 1000000 == 0: 
+                logging.debug("Iteration: " + str(i))
+            vals = line.split()
+            
+            userIndexer.append(vals[0])
+            movieIndexer.append(vals[1])
+            ratings.append(float(vals[2]))
+        
+        rowInds = userIndexer.getArray()
+        colInds = movieIndexer.getArray()
+        ratings = numpy.array(ratings)
+        
+        X = sppy.csarray((len(userIndexer.getIdDict()), len(movieIndexer.getIdDict())), storagetype="row", dtype=numpy.int)
+        X.put(numpy.array(ratings>3, numpy.int), numpy.array(rowInds, numpy.int32), numpy.array(colInds, numpy.int32), init=True)
+        X.prune()
+        X = SparseUtils.pruneMatrixRows(X, minNnzRows=10)
+        logging.debug("Read file: " + matrixFileName)
+        logging.debug("Non zero elements: " + str(X.nnz) + " shape: " + str(X.shape))
+        
+        return X 
