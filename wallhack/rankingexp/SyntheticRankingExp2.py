1407030bb8f4b2e9c4880664273179fbb76edb94
Charanpal Dhanjal <charanpal@gmail.com>
2014-06-09 17:28:15 +0100

    tidy up

--- wallhack/rankingexp/SyntheticRankingExp2.py
+++ wallhack/rankingexp/SyntheticRankingExp2.py
@@ -7,11 +7,9 @@ from sandbox.util.SparseUtils import SparseUtils
 import matplotlib 
 matplotlib.use("GTK3Agg")
 import matplotlib.pyplot as plt 
-from sandbox.util.ProfileUtils import ProfileUtils
-from sandbox.util.MCEvaluator import MCEvaluator
-from sandbox.util.Util import Util
+from sandbox.util.PathDefaults import PathDefaults
 from sandbox.util.Sampling import Sampling
-from sandbox.util.MCEvaluatorCython import MCEvaluatorCython
+from wallhack.rankingexp.DatasetUtils import DatasetUtils
 
 """
 How much random sampling do we need for fast convergence 
@@ -23,16 +21,9 @@ numpy.random.seed(21)
 numpy.set_printoptions(precision=3, suppress=True, linewidth=150)
 
 #Create a low rank matrix  
-m = 500
-n = 200
-k = 8 
-u = 20.0/n
-w = 1-u
-X, U, s, V = SparseUtils.generateSparseBinaryMatrix((m,n), k, w, csarray=True, verbose=True, indsPerRow=200)
-logging.debug("Number of non zero elements: " + str(X.nnz))
-logging.debug("Size of X: " + str(X.shape))
-
-U = U*s
+X, U, V = DatasetUtils.syntheticDataset1()
+outputFile = PathDefaults.getOutputDir() + "ranking/Exp9SyntheticResults.npz" 
+m, n = X.shape
 
 testSize = 5
 trainTestXs = Sampling.shuffleSplitRows(X, 1, testSize)
@@ -46,30 +37,26 @@ allOmegaPtr = SparseUtils.getOmegaListPtr(X)
 numRecordAucSamples = 200
 
 logging.debug("Number of non-zero elements: " + str((trainX.nnz, testX.nnz)))
-logging.debug("Train local AUC:" + str(MCEvaluator.localAUCApprox(trainOmegaPtr, U, V, w, numRecordAucSamples, allArray=allOmegaPtr)))
-logging.debug("Test local AUC:" + str(MCEvaluator.localAUCApprox(testOmegaPtr, U, V, w, numRecordAucSamples, allArray=allOmegaPtr)))
 
 #w = 1.0
-k2 = k
+k2 = 8
 u2 = 5.0/n
 w2 = 1-u2
 eps = 10**-15
-lmbda = 0
+lmbda = 1.0
 maxLocalAuc = MaxLocalAUC(k2, w2, eps=eps, lmbda=lmbda, stochastic=True)
 maxLocalAuc.maxIterations = 50
-maxLocalAuc.numRowSamples = 100
-maxLocalAuc.numStepIterations = 1000
+maxLocalAuc.numRowSamples = 10
 maxLocalAuc.numAucSamples = 10
 maxLocalAuc.numRecordAucSamples = 200
 maxLocalAuc.initialAlg = "rand"
-maxLocalAuc.recordStep = maxLocalAuc.numStepIterations
+maxLocalAuc.recordStep = 5
 maxLocalAuc.rate = "optimal"
 maxLocalAuc.alpha = 0.5
 maxLocalAuc.t0 = 0.0001
 maxLocalAuc.folds = 2
 maxLocalAuc.rho = 0.0
 maxLocalAuc.ks = numpy.array([k2])
-maxLocalAuc.testSize = 3
 maxLocalAuc.lmbdas = 2.0**-numpy.arange(0, 10, 2)
 #maxLocalAuc.numProcesses = 1
 maxLocalAuc.alphas = 2.0**-numpy.arange(0, 6, 1)
@@ -81,13 +68,13 @@ os.system('taskset -p 0xffffffff %d' % os.getpid())
 numRowSamplesArray = numpy.array([5, 10, 20, 50])
 numAucSamplesArray = numpy.array([5, 10, 20, 50])
 
-maxLocalAuc.numRowSamples = 100
-maxLocalAuc.numAucSamples = 100
+maxLocalAuc.numRowSamples = 10
+maxLocalAuc.numAucSamples = 10
 
 for numRowSamples in numRowSamplesArray: 
     maxLocalAuc.numRowSamples = numRowSamples
     logging.debug(maxLocalAuc)
-    U, V, trainObjs, trainAucs, testObjs, testAucs, iterations, time = maxLocalAuc.learnModel(trainX, testX=testX, verbose=True)
+    U, V, trainObjs, trainAucs, testObjs, testAucs, precisions, iterations, time = maxLocalAuc.learnModel(trainX, verbose=True)
     
     plt.figure(0)
     plt.plot(trainObjs, label="train nRow="+str(numRowSamples))
@@ -97,13 +84,13 @@ for numRowSamples in numRowSamplesArray:
     plt.plot(trainAucs, label="train nRow="+str(numRowSamples))
     #plt.plot(testAucs, label="test nRow="+str(numRowSamples))
 
-maxLocalAuc.numRowSamples = 100
-maxLocalAuc.numAucSamples = 100
+maxLocalAuc.numRowSamples = 10
+maxLocalAuc.numAucSamples = 10
 
 for numAucSamples in numAucSamplesArray: 
     maxLocalAuc.numAucSamples = numAucSamples
     logging.debug(maxLocalAuc)
-    U, V, trainObjs, trainAucs, testObjs, testAucs, iterations, time = maxLocalAuc.learnModel(trainX, testX=testX, verbose=True)
+    U, V, trainObjs, trainAucs, testObjs, testAucs, precisions, iterations, time = maxLocalAuc.learnModel(trainX, verbose=True)
 
     plt.figure(2)
     plt.plot(trainObjs, label="train nAuc="+str(numAucSamples))
--- wallhack/rankingexp/SyntheticRankingExp4.py
+++ wallhack/rankingexp/SyntheticRankingExp4.py
@@ -2,8 +2,7 @@ import numpy
 import logging
 import sys
 from sandbox.recommendation.MaxLocalAUC import MaxLocalAUC
-from sandbox.util.SparseUtils import SparseUtils
-from sandbox.util.MCEvaluator import MCEvaluator
+from wallhack.rankingexp.DatasetUtils import DatasetUtils
 from sandbox.util.Sampling import Sampling
 import matplotlib 
 matplotlib.use("GTK3Agg")
@@ -18,34 +17,25 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 numpy.set_printoptions(precision=3, suppress=True, linewidth=150)
 
 #Create a low rank matrix  
-m = 500
-n = 100
-k = 10 
-u = 0.2
-w = 1-u
-X, U, s, V = SparseUtils.generateSparseBinaryMatrix((m,n), k, w, csarray=True, verbose=True, indsPerRow=200)
-logging.debug("Number of non-zero elements: " + str(X.nnz))
-
-U = U*s
+X, U, V = DatasetUtils.syntheticDataset1()
+m, n = X.shape
 
 testSize = 5
 trainTestXs = Sampling.shuffleSplitRows(X, 1, testSize)
 trainX, testX = trainTestXs[0]
 
 logging.debug("Number of non-zero elements: " + str((trainX.nnz, testX.nnz)))
-#logging.debug("Total local AUC:" + str(MCEvaluator.localAUC(X, U, V, w)))
-#logging.debug("Train local AUC:" + str(MCEvaluator.localAUC(trainX, U, V, w)))
-#logging.debug("Test local AUC:" + str(MCEvaluator.localAUC(testX, U, V, w)))
 
-#w = 1.0
-k2 = k
+u = 5.0/n
+w = 1.0 - u
+k2 = 8
 eps = 10**-6
 maxLocalAuc = MaxLocalAUC(k2, w, eps=eps, stochastic=True)
 maxLocalAuc.maxIterations = 50
-maxLocalAuc.numRowSamples = 100
+maxLocalAuc.numRowSamples = 10
 maxLocalAuc.numAucSamples = 10
 maxLocalAuc.initialAlg = "rand"
-maxLocalAuc.recordStep = m*2
+maxLocalAuc.recordStep = 5
 maxLocalAuc.rate = "optimal"
 maxLocalAuc.alpha = 0.5
 maxLocalAuc.t0 = 10**-4
@@ -56,7 +46,7 @@ t0s = numpy.logspace(-3, -5, 6, base=10)
 for i, t0 in enumerate(t0s): 
     maxLocalAuc.t0 = t0
     logging.debug(maxLocalAuc)
-    U, V, trainObjs, trainAucs, testObjs, testAucs, ind, totalTime = maxLocalAuc.learnModel(trainX, verbose=True, testX=testX)
+    U, V, trainObjs, trainAucs, testObjs, testAucs, precisions, ind, totalTime = maxLocalAuc.learnModel(trainX, verbose=True)
     
     plt.figure(0)
     plt.plot(trainAucs, label="t0="+str(t0))
