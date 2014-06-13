4ee19886f90e7643e83e4d117fce68bfe526a0b3
Charanpal Dhanjal <charanpal@gmail.com>
2014-06-13 11:24:16 +0200

    simplify

--- wallhack/rankingexp/DatasetUtils.py
+++ wallhack/rankingexp/DatasetUtils.py
@@ -12,11 +12,10 @@ class DatasetUtils(object):
     """
     
     @staticmethod
-    def syntheticDataset1(): 
-        m = 500
-        n = 200
-        k = 8 
-        u = 20.0/n
+    def syntheticDataset1(m=500, n=200, k=8, u=0.1): 
+        """
+        Create a simple synthetic dataset 
+        """
         w = 1-u
         X, U, s, V = SparseUtils.generateSparseBinaryMatrix((m,n), k, w, csarray=True, verbose=True, indsPerRow=200)
         logging.debug("Non zero elements: " + str(X.nnz) + " shape: " + str(X.shape))
@@ -24,31 +23,7 @@ class DatasetUtils(object):
         
         return X, U, V
     
-    @staticmethod
-    def syntheticDataset2(): 
-        m = 100
-        n = 50
-        k = 8 
-        u = 20.0/n
-        w = 1-u
-        X, U, s, V = SparseUtils.generateSparseBinaryMatrix((m,n), k, w, csarray=True, verbose=True, indsPerRow=200)
-        logging.debug("Non zero elements: " + str(X.nnz) + " shape: " + str(X.shape))
-        U = U*s
-        
-        return X, U, V    
 
-    @staticmethod
-    def syntheticDataset3(): 
-        m = 5
-        n = 20
-        k = 8 
-        u = 10.0/n
-        w = 1-u
-        X, U, s, V = SparseUtils.generateSparseBinaryMatrix((m,n), k, w, csarray=True, verbose=True, indsPerRow=200)
-        logging.debug("Non zero elements: " + str(X.nnz) + " shape: " + str(X.shape))
-        U = U*s
-        
-        return X, U, V    
     
     @staticmethod
     def movieLens(): 
