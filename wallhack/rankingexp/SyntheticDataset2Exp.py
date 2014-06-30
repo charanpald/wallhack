a030212dd47b8eb3d0a5c54eb93abc47696bb795
Charanpal Dhanjal <charanpal@gmail.com>
2014-06-30 18:31:54 +0100

    Rename files

---
+++ wallhack/rankingexp/SyntheticDataset2Exp.py
@@ -0,0 +1,57 @@
+import numpy
+import logging
+import sys
+import argparse 
+import os
+import errno
+from wallhack.rankingexp.RankingExpHelper import RankingExpHelper
+from wallhack.rankingexp.DatasetUtils import DatasetUtils
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+numpy.random.seed(21)        
+numpy.set_printoptions(precision=3, suppress=True, linewidth=150)
+
+#Create a low rank matrix  
+X = DatasetUtils.syntheticDataset2()
+m, n = X.shape
+u = 5.0/n
+w = 1-u
+
+# Arguments related to the dataset
+dataArgs = argparse.Namespace()
+
+# Arguments related to the algorithm
+defaultAlgoArgs = argparse.Namespace()
+defaultAlgoArgs.u = 5/float(n) 
+
+# data args parser #
+dataParser = argparse.ArgumentParser(description="", add_help=False)
+dataParser.add_argument("-h", "--help", action="store_true", help="show this help message and exit")
+devNull, remainingArgs = dataParser.parse_known_args(namespace=dataArgs)
+if dataArgs.help:
+    helpParser  = argparse.ArgumentParser(description="", add_help=False, parents=[dataParser, RankingExpHelper.newAlgoParser(defaultAlgoArgs)])
+    helpParser.print_help()
+    exit()
+
+dataArgs.extendedDirName = ""
+dataArgs.extendedDirName += "SyntheticDataset2"
+
+# print args #
+logging.info("Running on " + dataArgs.extendedDirName)
+logging.info("Data params:")
+keys = list(vars(dataArgs).keys())
+keys.sort()
+for key in keys:
+    logging.info("    " + str(key) + ": " + str(dataArgs.__getattribute__(key)))
+
+logging.info("Creating the exp-runner")
+rankingExpHelper = RankingExpHelper(remainingArgs, defaultAlgoArgs, dataArgs.extendedDirName)
+rankingExpHelper.printAlgoArgs()
+#    os.makedirs(resultsDir, exist_ok=True) # for python 3.2
+try:
+    os.makedirs(rankingExpHelper.resultsDir)
+except OSError as err:
+    if err.errno != errno.EEXIST:
+        raise
+
+rankingExpHelper.runExperiment(X)
