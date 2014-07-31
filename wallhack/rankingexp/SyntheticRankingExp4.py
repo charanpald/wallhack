4766a96b69c8c974f4f7aff3e8b49e2eb418980a
Charanpal Dhanjal <charanpal@gmail.com>
2014-07-31 13:23:25 +0200

    Change file to find optimal learning rates for k and lambda

--- wallhack/rankingexp/SyntheticRankingExp4.py
+++ wallhack/rankingexp/SyntheticRankingExp4.py
@@ -3,13 +3,14 @@ import logging
 import sys
 from sandbox.recommendation.MaxLocalAUC import MaxLocalAUC
 from wallhack.rankingexp.DatasetUtils import DatasetUtils
+from sandbox.util.PathDefaults import PathDefaults 
 from sandbox.util.Sampling import Sampling
 import matplotlib 
 matplotlib.use("GTK3Agg")
 import matplotlib.pyplot as plt
 
 """
-Look at convergence with different rate/decay parameters. 
+Look at the optimal learning rate parameters for values of k, lambda 
 """
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -17,7 +18,30 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 numpy.set_printoptions(precision=3, suppress=True, linewidth=150)
 
 #Create a low rank matrix  
-X, U, V = DatasetUtils.syntheticDataset1()
+if len(sys.argv) > 1:
+    dataset = sys.argv[1]
+else: 
+    dataset = "synthetic"
+
+saveResults = True
+expNum = 4
+
+if dataset == "synthetic": 
+    X, U, V = DatasetUtils.syntheticDataset1()
+    outputFile = PathDefaults.getOutputDir() + "ranking/Exp" + str(expNum) + "SyntheticResults.npz" 
+elif dataset == "synthetic2": 
+    X = DatasetUtils.syntheticDataset2()
+    outputFile = PathDefaults.getOutputDir() + "ranking/Exp" + str(expNum) + "Synthetic2Results.npz" 
+elif dataset == "movielens": 
+    X = DatasetUtils.movieLens()
+    outputFile = PathDefaults.getOutputDir() + "ranking/Exp" + str(expNum) + "MovieLensResults.npz" 
+elif dataset == "flixster": 
+    X = DatasetUtils.flixster()
+    outputFile = PathDefaults.getOutputDir() + "ranking/Exp" + str(expNum) + "FlixsterResults.npz"  
+    X = Sampling.sampleUsers(X, 1000)
+else: 
+    raise ValueError("Unknown dataset: " + dataset)
+    
 m, n = X.shape
 
 testSize = 5
@@ -26,38 +50,60 @@ trainX, testX = trainTestXs[0]
 
 logging.debug("Number of non-zero elements: " + str((trainX.nnz, testX.nnz)))
 
-u = 5.0/n
+u = 0.1
 w = 1.0 - u
 k2 = 8
 eps = 10**-6
 maxLocalAuc = MaxLocalAUC(k2, w, eps=eps, stochastic=True)
 maxLocalAuc.maxIterations = 50
-maxLocalAuc.numRowSamples = 10
+maxLocalAuc.numRowSamples = 30
 maxLocalAuc.numAucSamples = 10
 maxLocalAuc.initialAlg = "rand"
-maxLocalAuc.recordStep = 5
+maxLocalAuc.recordStep = 10
 maxLocalAuc.rate = "optimal"
-maxLocalAuc.alpha = 0.5
-maxLocalAuc.t0 = 10**-4
+maxLocalAuc.rho = 0.5
+maxLocalAuc.folds = 1
 
-alphas = [0.1, 0.2, 0.5, 1.0]
-t0s = numpy.logspace(-3, -5, 6, base=10)
+ks = numpy.array([8, 16, 32])
+lmbdas = numpy.array([0.5, 1.0, 2.0, 4.0])
 
-for i, t0 in enumerate(t0s): 
-    maxLocalAuc.t0 = t0
-    logging.debug(maxLocalAuc)
-    U, V, trainObjs, trainAucs, testObjs, testAucs, precisions, ind, totalTime = maxLocalAuc.learnModel(trainX, verbose=True)
+optimalAlphas = numpy.zeros((ks.shape[0],lmbdas.shape[0]))
+optimalt0s = numpy.zeros((ks.shape[0], lmbdas.shape[0]))
+
+if saveResults:
+    
+    for i, k in enumerate(ks): 
+        for j, lmbda in enumerate(lmbdas):
+            maxLocalAuc.k = k 
+            maxLocalAuc.lmbdaV = lmbda             
+            
+            meanObjs = maxLocalAuc.learningRateSelect(X)
+            
+            optimalAlphas[i, j] = maxLocalAuc.alpha
+            optimalt0s[i, j] = maxLocalAuc.t0
+
+
+    numpy.savez(outputFile, optimalAlphas, optimalt0s)
+else: 
+    data = numpy.load(outputFile)
+    optimalAlphas, optimalt0s = data["arr_0"], data["arr_1"]
+    import matplotlib 
+    matplotlib.use("GTK3Agg")
+    import matplotlib.pyplot as plt 
     
     plt.figure(0)
-    plt.plot(trainAucs, label="t0="+str(t0))
-    plt.plot(testAucs, label="t0="+str(t0))
-    plt.legend()
+    plt.contourf(numpy.log2(ks), numpy.log2(lmbdas), optimalAlphas)
+    plt.xlabel("lambda")
+    plt.ylabel("k")
+    plt.colorbar()
     
     plt.figure(1)
-    plt.plot(trainObjs, label="t0="+str(t0))
-    plt.plot(testObjs, label="t0="+str(t0))
-    plt.legend()
+    plt.contourf(numpy.log2(ks), numpy.log2(lmbdas), optimalt0s)
+    plt.xlabel("lambda")
+    plt.ylabel("k")
+    plt.colorbar()
     
-plt.show()
-
-#Using the SVD as the initial solution gives a slightly better solution 
+    plt.show()
+    
+print(optimalAlphas)
+print(optimalt0s)
