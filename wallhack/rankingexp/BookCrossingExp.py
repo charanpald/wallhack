6171b7929ca2419bd329152e884b8f6f02bdcb3b
Charanpal Dhanjal <charanpal@gmail.com>
2014-10-07 21:45:21 +0200

    rename file

---
+++ wallhack/rankingexp/BookCrossingExp.py
@@ -0,0 +1,52 @@
+import numpy
+import logging
+import sys
+import argparse 
+from wallhack.rankingexp.RankingExpHelper import RankingExpHelper
+from wallhack.rankingexp.DatasetUtils import DatasetUtils
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+numpy.random.seed(21)        
+numpy.set_printoptions(precision=3, suppress=True, linewidth=150)
+
+# Arguments related to the dataset
+dataArgs = argparse.Namespace()
+
+# Arguments related to the algorithm
+defaultAlgoArgs = argparse.Namespace()
+defaultAlgoArgs.ks = numpy.array([64])
+defaultAlgoArgs.parallelSGD = True
+defaultAlgoArgs.recordFolds = 1
+defaultAlgoArgs.numRowSamples = 10
+defaultAlgoArgs.alphas = 2.0**-numpy.arange(3, 7)
+defaultAlgoArgs.lmbdasMlauc = 2.0**-numpy.arange(2, 6)
+
+# data args parser #
+dataParser = argparse.ArgumentParser(description="", add_help=False)
+dataParser.add_argument("-h", "--help", action="store_true", help="show this help message and exit")
+devNull, remainingArgs = dataParser.parse_known_args(namespace=dataArgs)
+if dataArgs.help:
+    helpParser  = argparse.ArgumentParser(description="", add_help=False, parents=[dataParser, RankingExpHelper.newAlgoParser(defaultAlgoArgs)])
+    helpParser.print_help()
+    exit()
+
+#Create/load a low rank matrix 
+X = DatasetUtils.bookCrossing(minNnzRows=10)
+(m, n) = X.shape
+
+
+dataArgs.extendedDirName = ""
+dataArgs.extendedDirName += "BookCrossing"
+
+# print args #
+logging.info("Running on " + dataArgs.extendedDirName)
+logging.info("Data params:")
+keys = list(vars(dataArgs).keys())
+keys.sort()
+for key in keys:
+    logging.info("    " + str(key) + ": " + str(dataArgs.__getattribute__(key)))
+
+logging.info("Creating the exp-runner")
+rankingExpHelper = RankingExpHelper(remainingArgs, defaultAlgoArgs, dataArgs.extendedDirName)
+rankingExpHelper.printAlgoArgs()
+rankingExpHelper.runExperiment(X)
