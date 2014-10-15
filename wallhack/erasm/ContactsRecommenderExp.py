93ae14fe0593d115e73e231228f3b74bb86cf363
Charanpal Dhanjal <charanpal@gmail.com>
2014-10-15 14:33:37 +0200

    tidy up: use argparse, and run 1 alg only

--- wallhack/erasm/ContactsRecommenderExp.py
+++ wallhack/erasm/ContactsRecommenderExp.py
@@ -4,6 +4,7 @@ import pickle
 import csv
 import sys 
 import os
+import argparse 
 from mrec.item_similarity.knn import CosineKNNRecommender
 from mrec.item_similarity.slim import SLIM
 from mrec.sparse import fast_sparse_matrix
@@ -17,16 +18,69 @@ from sandbox.util.Sampling import Sampling
 from wallhack.rankingexp.DatasetUtils import DatasetUtils 
 from wallhack.erasm.Evaluator import evaluate_against_contacts, evaluate_against_research_interests, read_contacts, read_interests, read_similar_authors
 
-
 """
 Use Mendeley author-documents and author-keywords to recommend contacts. 
-
+To run the parallel version of MLAUC you have to increase the amount of shared memory using 
+sudo sysctl -w kernel.shmmax=2147483648
 """
+
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-#Do model selection? 
+def saveResults(orderedItems, scores, dataset, similaritiesFileName, contactsFilename, interestsFilename, minScore, minContacts, minAcceptableSims): 
+    #Now let's write out the similarities file 
+    logging.debug("Generating recommendations for authors")
+    authorIndexerFilename = PathDefaults.getDataDir() + "reference/authorIndexer" + dataset + ".pkl"
+    authorIndexerFile = open(authorIndexerFilename)
+    authorIndexer = pickle.load(authorIndexerFile)
+    authorIndexerFile.close()
+    logging.debug("Loaded author indexer")
+    
+    reverseIndexer = authorIndexer.reverseTranslateDict()
+    
+    outputFile = open(similaritiesFileName, "w")
+    csvFile = csv.writer(outputFile, delimiter='\t')
+    
+    for i in range(orderedItems.shape[0]):
+        if i % 10000 == 0 : 
+            logging.debug("Iteration: " + str(i))
+            
+        row = [reverseIndexer[i]]                
+        
+        #Check author isn't recommended him/herself
+        for j in range(orderedItems.shape[1]): 
+            if orderedItems[i, j] != i:
+                row = [reverseIndexer[i], reverseIndexer[orderedItems[i, j]], scores[i, j]]
+            
+                csvFile.writerow(row)
+            
+    outputFile.close()
+    logging.debug("Wrote recommendations to " + similaritiesFileName)
+    
+    #Figure out how good the recommendations are on the contacts network                      
+    contacts = read_contacts(contactsFilename)
+    research_interests = read_interests(interestsFilename)
+    sims = read_similar_authors(similaritiesFileName, minScore)
+    
+    logging.debug('Evaluating against contacts...')
+    meanStatsContacts = evaluate_against_contacts(sims, contacts, minContacts)
+    
+    logging.debug('Evaluating against research interests...') 
+    meanStatsInterests = evaluate_against_research_interests(sims, research_interests, minAcceptableSims)
+    
+    logging.debug("Mean stats on contacts: " + str(meanStatsContacts))
+    logging.debug("Mean stats on interests:" + str(meanStatsInterests))
+    
+    return meanStatsContacts, meanStatsInterests 
 
-k = 128
+
+parser = argparse.ArgumentParser(description='Recommend coauthors')
+parser.add_argument("--modelSelect", action="store_true", help="Perform model selection (default: %(default)s)", default=False)
+parser.add_argument("--overwrite", action="store_true", help="Overwrite results (default: %(default)s)", default=False)
+parser.add_argument("--alg", type=str, help="Choice of algorithm (default: %(default)s)", default="SoftImpute")
+parser.add_argument("--k", type=int, help="Number of latent factors (default: %(default)s)", default=128)
+args = parser.parse_args()
+
+k = args.k
 maxItems = 10
 minScore = 0.0
 minContacts = 3
@@ -36,11 +90,20 @@ alpha = 0.2
 numProcesses = 2
 modelSelectSamples = 10**6
 
-modelSelect = True
+modelSelect = args.modelSelect
 folds = 3
 ks = numpy.array([32, 64, 128])
 rhosSi = numpy.linspace(1.0, 0.0, 5)
 
+overwrite = args.overwrite
+datasets = ["Keyword", "Document"]
+
+resultsDir = PathDefaults.getOutputDir() + "coauthors/"
+contactsFilename = PathDefaults.getDataDir() + "reference/contacts_anonymised.tsv"
+interestsFilename = PathDefaults.getDataDir() + "reference/author_interest"
+
+
+#Create all the recommendation algorithms
 softImpute = IterativeSoftImpute(k=k, postProcess=True, svdAlg="rsvd")
 softImpute.maxIterations = maxIterations
 softImpute.metric = "f1" 
@@ -69,147 +132,105 @@ knn = CosineKNNRecommender(kNeighbours)
 numFeatures = 200
 slim = SLIM(num_selected_features=numFeatures)
 
-"""
-To run the parallel version of MLAUC you have to increase the amount of shared memory using 
-sudo sysctl -w kernel.shmmax=2147483648
-"""
+learners = [("SoftImpute", softImpute), ("WRMF", wrmf), ("KNN", knn), ("MLAUC", maxLocalAuc), ("SLIM", slim)]
 
-overwrite = False
-datasets = ["Keyword", "Document"]
-#learners = [("SoftImpute", softImpute), ("WRMF", wrmf), ("KNN", knn)]
-#learners = [("MLAUC", maxLocalAuc)]
-learners = [("SoftImpute", softImpute)]
-#learners = [("SLIM", slim)]
-resultsDir = PathDefaults.getOutputDir() + "coauthors/"
-contactsFilename = PathDefaults.getDataDir() + "reference/contacts_anonymised.tsv"
-interestsFilename = PathDefaults.getDataDir() + "reference/author_interest"
+#Figure out the correct learner 
+for tempLearnerName, tempLearner in learners: 
+    if args.alg == tempLearnerName: 
+        learnerName = tempLearnerName
+        learner = tempLearner 
+
+if "learner" not in globals(): 
+    raise ValueError("Learner not found: " + learnerName)
 
 os.system('taskset -p 0xffffffff %d' % os.getpid())
 
 for dataset in datasets: 
     X = DatasetUtils.mendeley2(minNnzRows=0, dataset=dataset)
-    
-    for learnerName, learner in learners: 
-        outputFilename = resultsDir + "Results_" + learnerName + "_" + dataset + ".npz"  
-        similaritiesFileName = resultsDir + "Recommendations_" + learnerName + "_" + dataset + ".csv" 
-        fileLock = FileLock(outputFilename)  
-            
-        if not (fileLock.isLocked() or fileLock.fileExists()) or overwrite: 
-            fileLock.lock()       
-            
-            logging.debug(learner)      
-        
-            try: 
-                #Do some recommendation 
-                if type(learner) == IterativeSoftImpute:  
-                    trainX = X.toScipyCsc()
-                    trainIterator = iter([trainX])
-                             
-                    if modelSelect: 
-                        modelSelectX, userInds = Sampling.sampleUsers2(X, modelSelectSamples)
-                        modelSelectX = modelSelectX.toScipyCsc()                            
-                        cvInds = Sampling.randCrossValidation(folds, modelSelectX.nnz)
-                        meanMetrics, stdMetrics = learner.modelSelect2(modelSelectX, rhosSi, ks, cvInds)
-                    
-                    ZList = learner.learnModel(trainIterator)    
-                    U, s, V = ZList.next()
-                    U = U*s
-                elif type(learner) == WeightedMf:  
-                    trainX = X.toScipyCsr()
 
-                    if modelSelect:                     
-                        modelSelectX, userInds = Sampling.sampleUsers2(X, modelSelectSamples)
-                        modelSelectX = modelSelectX.toScipyCsc()  
-                        meanMetrics, stdMetrics = learner.modelSelect(modelSelectX)                          
-                    
-                    learner.learnModel(trainX)
-                    U = learner.U 
-                    V = learner.V
-                elif type(learner) == CosineKNNRecommender or type(learner) == SLIM: 
-                    fastTrainX = fast_sparse_matrix(X.toScipyCsr())
-                    trainX = X.toScipyCsr()
-                    m, n = trainX.shape
-                    learner.fit(fastTrainX)
-                    
-                    recommendations = learner.range_recommend_items(trainX, 0, m, max_items=maxItems)
-                    
-                    orderedItems = numpy.zeros((m, maxItems), numpy.int)
-                    scores = numpy.zeros((m, maxItems))
-                    
-                    for i in range(m):
-                        itemScores = numpy.array(recommendations[i])
-                        if itemScores.shape[0] != 0: 
-                            orderedItems[i, 0:itemScores.shape[0]] =  itemScores[:, 0]
-                            scores[i, 0:itemScores.shape[0]] = itemScores[:, 1]
-                        
-                else: 
-                    trainX = X
-                    
-                    if modelSelect: 
-                        modelSelectX, userInds = Sampling.sampleUsers2(trainX, modelSelectSamples)
-                        meanMetrics, stdMetrics = learner.modelSelect(modelSelectX)
-                    
-                    learner.learnModel(X)
-                    U = learner.U 
-                    V = learner.V 
-                
-                if type(learner) != CosineKNNRecommender:
-                    U = numpy.ascontiguousarray(U)
-                    V = numpy.ascontiguousarray(V)
-                    
-                    #Note that we compute UU^T for recommendations 
-                    orderedItems, scores = MCEvaluator.recommendAtk(U, U, maxItems, verbose=True)
-                
-                #Normalise scores 
-                scores /= numpy.max(scores)                
-                
-                #Now let's write out the similarities file 
-                logging.debug("Generating recommendations for authors")
-                authorIndexerFilename = PathDefaults.getDataDir() + "reference/authorIndexer" + dataset + ".pkl"
-                authorIndexerFile = open(authorIndexerFilename)
-                authorIndexer = pickle.load(authorIndexerFile)
-                authorIndexerFile.close()
-                logging.debug("Loaded author indexer")
-                
-                reverseIndexer = authorIndexer.reverseTranslateDict()
-                
-                outputFile = open(similaritiesFileName, "w")
-                csvFile = csv.writer(outputFile, delimiter='\t')
-                
-                for i in range(orderedItems.shape[0]):
-                    if i % 10000 == 0 : 
-                        logging.debug("Iteration: " + str(i))
-                        
-                    row = [reverseIndexer[i]]                
-                    
-                    #Check author isn't recommended him/herself
-                    for j in range(orderedItems.shape[1]): 
-                        if orderedItems[i, j] != i:
-                            row = [reverseIndexer[i], reverseIndexer[orderedItems[i, j]], scores[i, j]]
-                        
-                            csvFile.writerow(row)
-                        
-                outputFile.close()
-                logging.debug("Wrote recommendations to " + similaritiesFileName)
-                
-                #Figure out how good the recommendations are on the contacts network                      
-                contacts = read_contacts(contactsFilename)
-                research_interests = read_interests(interestsFilename)
-                sims = read_similar_authors(similaritiesFileName, minScore)
-                
-                logging.debug('Evaluating against contacts...')
-                meanStatsContacts = evaluate_against_contacts(sims, contacts, minContacts)
-                
-                logging.debug('Evaluating against research interests...') 
-                meanStatsInterests = evaluate_against_research_interests(sims, research_interests, minAcceptableSims)
-                
-                logging.debug("Mean stats on contacts: " + str(meanStatsContacts))
-                logging.debug("Mean stats on interests:" + str(meanStatsInterests))
-                
-                numpy.savez(outputFilename, meanStatsContacts, meanStatsInterests)
-                logging.debug("Saved precisions/recalls on contacts/interests as " + outputFilename)
+    outputFilename = resultsDir + "Results_" + learnerName + "_" + dataset + ".npz"  
+    similaritiesFileName = resultsDir + "Recommendations_" + learnerName + "_" + dataset + ".csv" 
+    fileLock = FileLock(outputFilename)  
         
-            finally: 
-                fileLock.unlock()
-        else: 
-            logging.debug("File is locked or already computed: " + outputFilename)      
+    if not (fileLock.isLocked() or fileLock.fileExists()) or overwrite: 
+        fileLock.lock()       
+        
+        logging.debug(learner)      
+    
+        try: 
+            #Do some recommendation 
+            if type(learner) == IterativeSoftImpute:  
+                trainX = X.toScipyCsc()
+                trainIterator = iter([trainX])
+                         
+                if modelSelect: 
+                    modelSelectX, userInds = Sampling.sampleUsers2(X, modelSelectSamples)
+                    modelSelectX = modelSelectX.toScipyCsc()                            
+                    cvInds = Sampling.randCrossValidation(folds, modelSelectX.nnz)
+                    meanMetrics, stdMetrics = learner.modelSelect2(modelSelectX, rhosSi, ks, cvInds)
+                
+                ZList = learner.learnModel(trainIterator)    
+                U, s, V = ZList.next()
+                U = U*s
+            elif type(learner) == WeightedMf:  
+                trainX = X.toScipyCsr()
+
+                if modelSelect:                     
+                    modelSelectX, userInds = Sampling.sampleUsers2(X, modelSelectSamples)
+                    modelSelectX = modelSelectX.toScipyCsc()  
+                    meanMetrics, stdMetrics = learner.modelSelect(modelSelectX)                          
+                
+                learner.learnModel(trainX)
+                U = learner.U 
+                V = learner.V
+            elif type(learner) == CosineKNNRecommender or type(learner) == SLIM: 
+                fastTrainX = fast_sparse_matrix(X.toScipyCsr())
+                trainX = X.toScipyCsr()
+                m, n = trainX.shape
+                learner.fit(fastTrainX)
+                
+                recommendations = learner.range_recommend_items(trainX, 0, m, max_items=maxItems)
+                
+                orderedItems = numpy.zeros((m, maxItems), numpy.int)
+                scores = numpy.zeros((m, maxItems))
+                
+                for i in range(m):
+                    itemScores = numpy.array(recommendations[i])
+                    if itemScores.shape[0] != 0: 
+                        orderedItems[i, 0:itemScores.shape[0]] =  itemScores[:, 0]
+                        scores[i, 0:itemScores.shape[0]] = itemScores[:, 1]
+            else: 
+                trainX = X
+                
+                if modelSelect: 
+                    modelSelectX, userInds = Sampling.sampleUsers2(trainX, modelSelectSamples)
+                    meanMetrics, stdMetrics = learner.modelSelect(modelSelectX)
+                
+                learner.learnModel(X)
+                U = learner.U 
+                V = learner.V 
+            
+            if type(learner) != CosineKNNRecommender:
+                U = numpy.ascontiguousarray(U)
+                V = numpy.ascontiguousarray(V)
+                
+                #Note that we compute UU^T for recommendations 
+                orderedItems, scores = MCEvaluator.recommendAtk(U, U, maxItems, verbose=True)
+                orderedItems2, scores2 = MCEvaluator.recommendAtk(U.dot(V.T.dot(V)), U, maxItems, verbose=True)
+            else: 
+                orderedItems2 = orderedItems 
+                scores2 = scores 
+            
+            #Normalise scores 
+            scores /= numpy.max(scores)                
+            
+            meanStatsContacts, meanStatsInterests = saveResults(orderedItems, scores, dataset, similaritiesFileName, contactsFilename, interestsFilename, minScore, minContacts, minAcceptableSims)
+            meanStatsContacts2, meanStatsInterests2 = saveResults(orderedItems2, scores2, dataset, similaritiesFileName, contactsFilename, interestsFilename, minScore, minContacts, minAcceptableSims)
+
+            numpy.savez(outputFilename, meanStatsContacts, meanStatsInterests, meanStatsContacts2, meanStatsInterests2)
+            logging.debug("Saved precisions/recalls on contacts/interests as " + outputFilename)
+    
+        finally: 
+            fileLock.unlock()
+    else: 
+        logging.debug("File is locked or already computed: " + outputFilename)      
