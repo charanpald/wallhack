7b6128758e5ae3c2185d6efca91fbb137bc773e5
Charanpal Dhanjal <charanpal@gmail.com>
2014-10-17 13:26:40 +0200

    Add epinions

--- wallhack/rankingexp/DatasetUtils.py
+++ wallhack/rankingexp/DatasetUtils.py
@@ -3,6 +3,7 @@ import logging
 import sppy 
 import sppy.io
 import array 
+import scipy.io
 from sandbox.util.SparseUtils import SparseUtils
 from sandbox.util.SparseUtilsCython import SparseUtilsCython
 from sandbox.util.PathDefaults import PathDefaults 
@@ -43,7 +44,7 @@ class DatasetUtils(object):
         return X   
     
     @staticmethod
-    def movieLens(minNnzRows=10, minNnzCols=10, quantile=90): 
+    def movieLens(minNnzRows=10, minNnzCols=2, quantile=90): 
         matrixFileName = PathDefaults.getDataDir() + "movielens/ml-100k/u.data" 
         data = numpy.loadtxt(matrixFileName)
         X = sppy.csarray((numpy.max(data[:, 0]), numpy.max(data[:, 1])), storagetype="row", dtype=numpy.int)
@@ -54,14 +55,14 @@ class DatasetUtils(object):
         #maxNnz = numpy.percentile(X.sum(0), quantile)
         #X = SparseUtils.pruneMatrixCols(X, minNnz=minNnzCols, maxNnz=maxNnz)
         X = SparseUtils.pruneMatrixRows(X, minNnzRows=minNnzRows)
-        X = SparseUtils.pruneMatrixCols(X, minNnz=1)
+        X = SparseUtils.pruneMatrixCols(X, minNnz=minNnzCols)
         logging.debug("Read file: " + matrixFileName)
         logging.debug("Non zero elements: " + str(X.nnz) + " shape: " + str(X.shape))
 
         return X
         
     @staticmethod 
-    def flixster(minNnzRows=10, minNnzCols=10, quantile=90): 
+    def flixster(minNnzRows=10, minNnzCols=2, quantile=90): 
         matrixFileName = PathDefaults.getDataDir() + "flixster/Ratings.timed.txt" 
         matrixFile = open(matrixFileName)
         matrixFile.readline()
@@ -91,7 +92,7 @@ class DatasetUtils(object):
         #maxNnz = numpy.percentile(X.sum(0), quantile)
         #X = SparseUtils.pruneMatrixCols(X, minNnz=minNnzCols, maxNnz=maxNnz)
         X = SparseUtils.pruneMatrixRows(X, minNnzRows=minNnzRows)
-        X = SparseUtils.pruneMatrixCols(X, minNnz=1)
+        X = SparseUtils.pruneMatrixCols(X, minNnz=minNnzCols)
         
         logging.debug("Read file: " + matrixFileName)
         logging.debug("Non zero elements: " + str(X.nnz) + " shape: " + str(X.shape))
@@ -101,7 +102,7 @@ class DatasetUtils(object):
         return X 
 
     @staticmethod         
-    def mendeley(minNnzRows=10, minNnzCols=10, quantile=90, dataset="Doc", sigma=0.05, indicator=True):
+    def mendeley(minNnzRows=10, minNnzCols=2, quantile=90, dataset="Doc", sigma=0.05, indicator=True):
         authorAuthorFileName = PathDefaults.getDataDir() + "reference/authorAuthor"+ dataset + "Matrix_sigma=" + str(sigma) + ".mtx"
         logging.debug("Reading file: " + authorAuthorFileName)
         X = sppy.io.mmread(authorAuthorFileName, storagetype="row")
@@ -115,7 +116,7 @@ class DatasetUtils(object):
         #maxNnz = numpy.percentile(X.sum(0), quantile)
         #X = SparseUtils.pruneMatrixCols(X, minNnz=minNnzCols, maxNnz=maxNnz)
         X = SparseUtils.pruneMatrixRows(X, minNnzRows=minNnzRows) 
-        X = SparseUtils.pruneMatrixCols(X, minNnz=1)
+        X = SparseUtils.pruneMatrixCols(X, minNnz=minNnzCols)
         
         logging.debug("Read file: " + authorAuthorFileName)
         logging.debug("Non-zero elements: " + str(X.nnz) + " shape: " + str(X.shape))
@@ -123,7 +124,7 @@ class DatasetUtils(object):
         return X
         
     @staticmethod         
-    def mendeley2(minNnzRows=10, minNnzCols=10, quantile=90, dataset="Document"):
+    def mendeley2(minNnzRows=10, minNnzCols=2, quantile=90, dataset="Document"):
         authorAuthorFileName = PathDefaults.getDataDir() + "reference/author" + dataset + "Matrix.mtx"
         logging.debug("Reading file: " + authorAuthorFileName)
         X = sppy.io.mmread(authorAuthorFileName, storagetype="row")
@@ -133,7 +134,7 @@ class DatasetUtils(object):
         #maxNnz = numpy.percentile(X.sum(0), quantile)
         #X = SparseUtils.pruneMatrixCols(X, minNnz=minNnzCols, maxNnz=maxNnz)
         X = SparseUtils.pruneMatrixRows(X, minNnzRows=minNnzRows)
-        X = SparseUtils.pruneMatrixCols(X, minNnz=1)
+        X = SparseUtils.pruneMatrixCols(X, minNnz=minNnzCols)
         
         logging.debug("Read file: " + authorAuthorFileName)
         logging.debug("Non-zero elements: " + str(X.nnz) + " shape: " + str(X.shape))
@@ -142,7 +143,7 @@ class DatasetUtils(object):
         
         
     @staticmethod 
-    def bookCrossing(minNnzRows=10, minNnzCols=10, quantile=90): 
+    def bookCrossing(minNnzRows=10, minNnzCols=2, quantile=90): 
         matrixFileName = PathDefaults.getDataDir() + "book-crossing/BX-Book-Ratings.csv" 
         matrixFile = open(matrixFileName)
         matrixFile.readline()
@@ -174,7 +175,37 @@ class DatasetUtils(object):
         X.prune()
         
         X = SparseUtils.pruneMatrixRows(X, minNnzRows=minNnzRows)
-        X = SparseUtils.pruneMatrixCols(X, minNnz=1)
+        X = SparseUtils.pruneMatrixCols(X, minNnz=minNnzCols)
+        
+        logging.debug("Read file: " + matrixFileName)
+        logging.debug("Non zero elements: " + str(X.nnz) + " shape: " + str(X.shape))
+
+        return X 
+        
+    @staticmethod 
+    def epinions(minNnzRows=10, minNnzCols=2, quantile=90): 
+        matrixFileName = PathDefaults.getDataDir() + "epinions/rating.mat" 
+        A = scipy.io.loadmat(matrixFileName)["rating"]
+        
+        userIndexer = IdIndexer("i")
+        itemIndexer = IdIndexer("i")        
+        
+        for i in range(A.shape[0]): 
+            userIndexer.append(A[i, 0])
+            itemIndexer.append(A[i, 1])
+
+
+        rowInds = userIndexer.getArray()
+        colInds = itemIndexer.getArray()
+        ratings = A[:, 3]        
+        
+        X = sppy.csarray((len(userIndexer.getIdDict()), len(itemIndexer.getIdDict())), storagetype="row", dtype=numpy.int)
+        X.put(numpy.array(ratings>3, numpy.int), numpy.array(rowInds, numpy.int32), numpy.array(colInds, numpy.int32), init=True)
+        X.prune()
+        
+        
+        X = SparseUtils.pruneMatrixRows(X, minNnzRows=minNnzRows)
+        X = SparseUtils.pruneMatrixCols(X, minNnz=minNnzCols)
         
         logging.debug("Read file: " + matrixFileName)
         logging.debug("Non zero elements: " + str(X.nnz) + " shape: " + str(X.shape))
