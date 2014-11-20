0f60e3f09ff70f8aee7ea70872202fbd67ac5c02
Charanpal Dhanjal <charanpal@gmail.com>
2014-11-20 10:43:24 +0100

    Param tweak

--- wallhack/rankingexp/BookCrossingExp.py
+++ wallhack/rankingexp/BookCrossingExp.py
@@ -18,8 +18,8 @@ defaultAlgoArgs.ks = numpy.array([64])
 #defaultAlgoArgs.parallelSGD = True
 defaultAlgoArgs.recordFolds = 1
 defaultAlgoArgs.numRowSamples = 10
-defaultAlgoArgs.alphas = 2.0**-numpy.arange(3, 7)
-defaultAlgoArgs.lmbdasMlauc = 2.0**-numpy.arange(2, 6)
+defaultAlgoArgs.alphas = 2.0**-numpy.arange(1, 6)
+defaultAlgoArgs.lmbdasMlauc = 2.0**-numpy.arange(2, 8)
 
 # data args parser #
 dataParser = argparse.ArgumentParser(description="", add_help=False)
