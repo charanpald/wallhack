2e815bce709fe94b522efb5b8a5e2666c195a587
Charanpal Dhanjal <charanpal@gmail.com>
2014-11-24 11:04:38 +0100

    param tweaks

--- wallhack/rankingexp/FlixsterExp.py
+++ wallhack/rankingexp/FlixsterExp.py
@@ -15,7 +15,7 @@ dataArgs = argparse.Namespace()
 # Arguments related to the algorithm
 defaultAlgoArgs = argparse.Namespace()
 defaultAlgoArgs.ks = numpy.array([64])
-defaultAlgoArgs.parallelSGD = True
+#defaultAlgoArgs.parallelSGD = True
 defaultAlgoArgs.recordFolds = 1
 defaultAlgoArgs.numRowSamples = 10
 defaultAlgoArgs.alphas = 2.0**-numpy.arange(1, 6)
