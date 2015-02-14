b798857f4f25e1d2bcb90992f9cb7064ab79766e
Charanpal <charanpal@gmail.com>
2015-02-14 10:43:15 +0000

    param tweaks

--- wallhack/rankingexp/LearningRateExp.py
+++ wallhack/rankingexp/LearningRateExp.py
@@ -18,37 +18,34 @@ if len(sys.argv) > 1:
 else: 
     dataset = "flixster"
 
-saveResults = False
+saveResults = True
 prefix = "LearningRate"
 outputFile = PathDefaults.getOutputDir() + "ranking/" + prefix + dataset.title() + "Results.npz" 
-X = DatasetUtils.getDataset(dataset)
-m, n = X.shape
 
 k2 = 64
-u2 = 5/float(n)
-w2 = 1-u2
+w2 = 0.9
 eps = 10**-8
 lmbda = 0.01
 maxLocalAuc = MaxLocalAUC(k2, w2, eps=eps, lmbdaU=0.1, lmbdaV=0.1, stochastic=True)
 maxLocalAuc.alpha = 50
-maxLocalAuc.alphas = 2.0**-numpy.arange(0, 5, 1)
+maxLocalAuc.alphas = 2.0**numpy.arange(3, 10)
 maxLocalAuc.beta = 2
 maxLocalAuc.bound = False
 maxLocalAuc.delta = 0.1
 maxLocalAuc.eta = 0
 maxLocalAuc.folds = 2
-maxLocalAuc.initialAlg = "rand"
+maxLocalAuc.initialAlg = "svd"
 maxLocalAuc.itemExpP = 0.0
 maxLocalAuc.itemExpQ = 0.0
 maxLocalAuc.ks = numpy.array([4, 8, 16, 32, 64, 128])
-maxLocalAuc.lmbdas = numpy.linspace(0.5, 2.0, 7)
+maxLocalAuc.lmbdas = 2.0**-numpy.arange(1, 7)
 maxLocalAuc.loss = "hinge" 
-maxLocalAuc.maxIterations = 100
+maxLocalAuc.maxIterations = 500
 maxLocalAuc.maxNorm = 100
 maxLocalAuc.metric = "f1"
 maxLocalAuc.normalise = False
 maxLocalAuc.numAucSamples = 10
-maxLocalAuc.numProcesses = 1
+maxLocalAuc.numProcesses = multiprocessing.cpu_count()
 maxLocalAuc.numRecordAucSamples = 200
 maxLocalAuc.numRowSamples = 30
 maxLocalAuc.rate = "constant"
@@ -58,7 +55,7 @@ maxLocalAuc.rho = 1.0
 maxLocalAuc.t0 = 1.0
 maxLocalAuc.t0s = 2.0**-numpy.arange(7, 12, 1)
 maxLocalAuc.validationSize = 5
-maxLocalAuc.validationUsers = 1.0
+maxLocalAuc.validationUsers = 0.0
 
 if saveResults: 
     X = DatasetUtils.getDataset(dataset, nnz=1000000)
@@ -73,11 +70,11 @@ if saveResults:
     print(X4.shape, X4.nnz)  
     print(X5.shape, X5.nnz)   
     
-    meanObjs1, stdObjs1 = maxLocalAuc.learningRateSelect(X)
-    meanObjs2, stdObjs2 = maxLocalAuc.learningRateSelect(X2)
-    meanObjs3, stdObjs3 = maxLocalAuc.learningRateSelect(X3)
-    meanObjs4, stdObjs4 = maxLocalAuc.learningRateSelect(X4)
-    meanObjs5, stdObjs5 = maxLocalAuc.learningRateSelect(X5)
+    meanObjs1, paramDict = maxLocalAuc.learningRateSelect(X)
+    meanObjs2, paramDict = maxLocalAuc.learningRateSelect(X2)
+    meanObjs3, paramDict = maxLocalAuc.learningRateSelect(X3)
+    meanObjs4, paramDict = maxLocalAuc.learningRateSelect(X4)
+    meanObjs5, paramDict = maxLocalAuc.learningRateSelect(X5)
 
     numpy.savez(outputFile, meanObjs1, meanObjs2, meanObjs3, meanObjs4, meanObjs5)
 else: 
